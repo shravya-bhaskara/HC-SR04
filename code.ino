6fd57a4d0f93e2ed0f28b9ce08359cdb6a907fd5
shravya-bhaskara <59280388+shravya-bhaskara@users.noreply.github.com>
2020-05-26 22:53:51 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,12 +1,17 @@
+
 #define trig_pin 9  //since trigger pin is connected to digital pin 9
 #define echo_pin 11 //since echo pin is connected to digital pin 11
+
 int led = 3;        //led connected to digital pin 3
 long t;             // variable to measure time taken (t)
 int d;              // variable to measure distance between the sensor and the object 
+
 void setup() {
   // put your setup code here, to run once:
+  
 pinMode(trig_pin, OUTPUT);  //trigger pin is an output pin
 pinMode(echo_pin, INPUT);   //echo pin is an input pin
+  
 pinMode(led, OUTPUT);       
 
 Serial.begin(9600);         //starting communication with the serial monitor
@@ -15,6 +20,7 @@ Serial.begin(9600);         //starting communication with the serial monitor
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
 digitalWrite(trig_pin, LOW);
 delayMicroseconds(2);
 
@@ -30,6 +36,7 @@ d = t*(0.034/2);                   /*speed of sound in air is 340 m/s. since tim
                                    thus, the distance calcuted is divided by 2, so as to get the original lentgh between the sensor and the obstacle. */
 
 if(d<=50){                        // if distance is less than or equal to 50 cm 
+  
   Serial.print("distance: ");
   Serial.print(d);
   Serial.print(" cm \n");
@@ -37,6 +44,7 @@ if(d<=50){                        // if distance is less than or equal to 50 cm
   digitalWrite(led, HIGH);        //led glows bright
   
 }else{                            //if distance is greater than 50 cm
+  
   Serial.print("distance: ");   
   Serial.print(d);
   Serial.print(" cm \n");
